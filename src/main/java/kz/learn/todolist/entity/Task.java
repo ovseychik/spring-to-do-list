ea4b4387c8f0e408c2c957261f168172ac111d90
ovseychiks <ovseychiks@kacd.kz>
2024-03-09 22:27:03 +0500

    Fixes imports (bumped up limit for wildcards)

--- src/main/java/kz/learn/todolist/entity/Task.java
+++ src/main/java/kz/learn/todolist/entity/Task.java
@@ -1,6 +1,10 @@
 package kz.learn.todolist.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 
 import java.time.LocalDateTime;
 
