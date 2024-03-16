195af6685c1600e8526066517a6c988e7c31de39
ovseychiks <ovseychiks@kacd.kz>
2024-03-16 18:08:59 +0500

    Adds Spring Security dependency and User entity

---
+++ src/main/java/kz/learn/todolist/entity/User.java
@@ -0,0 +1,39 @@
+package kz.learn.todolist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String Password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+}
