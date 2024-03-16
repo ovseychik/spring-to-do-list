4664640556a393f9dc35332a8ced37009c51b834
ovseychiks <ovseychiks@kacd.kz>
2024-03-16 18:11:40 +0500

    Adds User repository

---
+++ src/main/java/kz/learn/todolist/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package kz.learn.todolist.repository;
+
+import kz.learn.todolist.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
