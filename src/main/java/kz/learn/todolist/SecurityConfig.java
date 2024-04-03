e57e2da8ec29dd0b1572374c222362b08def7f95
ovseychiks <ovseychiks@kacd.kz>
2024-04-03 21:35:27 +0500

    Creates users and authorities entities

--- src/main/java/kz/learn/todolist/SecurityConfig.java
+++ src/main/java/kz/learn/todolist/SecurityConfig.java
@@ -4,14 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -46,7 +42,7 @@ public class SecurityConfig {
     @Value("${adminpassword}")
     private String adminPassword;
 
-    @Bean
+   /* @Bean
     DataSource datasource() {
         DataSource dataSource = DataSourceBuilder.create()
                 .driverClassName(driverClassName)
@@ -56,7 +52,7 @@ public class SecurityConfig {
                 .build();
         return dataSource;
     }
-
+*/
     @Bean
     JdbcUserDetailsManager users(DataSource dataSource, PasswordEncoder encoder) {
         UserDetails admin = User.builder()
---
+++ src/main/java/kz/learn/todolist/entity/Users.java
@@ -0,0 +1,51 @@
+package kz.learn.todolist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String password;
+    private Boolean enabled;
+
+    public Users() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+}
